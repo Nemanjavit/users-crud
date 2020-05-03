3901e8c0b1df8a696556c8cbd1be6bd42672c64e
NemanjaVit <vitnemanja3@gmail.com>
2020-05-03 10:46:59 +0200

    dashboard visual done

--- users-crud/src/App.js
+++ users-crud/src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import SignUpPage from "./components/SignUpPage";
 import SignInPage from "./components/SignInPage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Route path="/dashboard" component={Dashboard} />
         <Route path="/signup" component={SignUpPage} />
         <Route path="/signin" component={SignInPage} />
       </div>
---
+++ users-crud/src/components/Dashboard.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <div className="container">
+        <h3>Dashboard</h3>
+        <div className="row justify-content-around p-5">
+          <div className="col-3 shadow p-3  bg-white rounded">
+            <i className="fas fa-list-ol fa-3x"></i>
+            <p>100</p>
+            <span>total post</span>
+          </div>
+          <div className="col-3 shadow p-3  bg-white rounded mx-4">
+            <i className="far fa-comments fa-3x"></i>
+            <p>123</p>
+            <p>total comments</p>
+          </div>
+          <div className="col-3 shadow p-3  bg-white rounded">
+            <i className="fas fa-user-friends fa-3x"></i>
+            <p>2</p>
+            <p>Users</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
