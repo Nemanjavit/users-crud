41dacc67aaf49d6d6503929935a5289c597774a8
NemanjaVit <vitnemanja3@gmail.com>
2020-05-05 10:43:19 +0200

    fix

--- users-crud/src/components/Header.js
+++ users-crud/src/components/Header.js
@@ -1,18 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { getToken } from "./Helpers/token";
 
 const Header = () => {
+  const [token, setToken] = useState();
+  const location = useLocation();
+  useEffect(() => {
+    setToken(getToken());
+  }, [token, location]);
   return (
     <header className="bg-dark">
       <nav className="navbar navbar-dark container px-5">
         <span className="navbar-brand mb-0 h1">Navbar</span>
         <div>
-          <Link className="text-white px-2" to="/posts">
-            Posts
-          </Link>
-          <Link className="text-white px-2" to="/about">
-            About
-          </Link>
+          {token ? (
+            <div>
+              <Link to="/myposts" className="text-white">
+                My Posts
+              </Link>
+              <Link to="/dashboard" className="text-white">
+                Dashboard
+              </Link>
+            </div>
+          ) : (
+            <div>
+              <Link className="text-white px-2" to="/posts">
+                Posts
+              </Link>
+              <Link className="text-white px-2" to="/about">
+                About
+              </Link>
+            </div>
+          )}
         </div>
       </nav>
     </header>
